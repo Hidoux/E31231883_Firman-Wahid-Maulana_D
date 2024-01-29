50201012b54e09c9d06c92a419276c4f060b7390
Hidoux <hidoujin@gmail.com>
2024-01-29 18:45:06 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Firman Wahid Maulana");
+        System.out.println("Hello, Dzakiyyan Nasyatha Adlin");
+
+    }
+
+}
